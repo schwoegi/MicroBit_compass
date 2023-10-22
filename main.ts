2dc29cb70dfaa7404c13dce9dcd9edf12c77643d
schwoegi <16440128+schwoegi@users.noreply.github.com>
2023-10-22 20:21:51 +0200

    Audio on start

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
 let degrees = 0
-if (degrees < 45) {
-    basic.showString("N")
-} else if (degrees < 135) {
-    basic.showString("E")
-} else if (degrees < 225) {
-    basic.showString("S")
-} else if (degrees < 315) {
-    basic.showString("W")
-} else {
-    basic.showString("N")
-}
+music.play(music.stringPlayable("C C E G C G E G ", 240), music.PlaybackMode.InBackground)
 basic.forever(function () {
     degrees = input.compassHeading()
+    if (degrees < 45) {
+        basic.showString("N")
+    } else if (degrees < 135) {
+        basic.showString("E")
+    } else if (degrees < 225) {
+        basic.showString("S")
+    } else if (degrees < 315) {
+        basic.showString("W")
+    } else {
+        basic.showString("N")
+    }
 })
